d36c303733c26b58602638ef1803f5c6b9fa1d06
nktoan <53964833+nktoan@users.noreply.github.com>
2025-08-10 13:24:19 +0000

    Deploying to gh-pages from @ nktoan/nktoan.github.io@8f6e7d2e1f633e6f828bbf5a699922a226f138b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+        },{id: "nav-",
+          title: "",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
