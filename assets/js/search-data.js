5ead4483b766371e623857a7fd1f85d81e3b73a1
nktoan <53964833+nktoan@users.noreply.github.com>
2025-08-10 14:17:23 +0000

    Deploying to gh-pages from @ nktoan/nktoan.github.io@e74597359fbef9f48d0d7a541c6b181f7dc28fd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-repositories",
+          title: "Repositories",
           description: "",
           section: "Navigation",
           handler: () => {
@@ -41,63 +41,13 @@ ninja.data = [{
             window.location.href = "/blog/2025/inference-first-then-training/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-our-paper-on-image-editing-with-diffusion-models-for-which-i-am-the-lead-author-has-been-accepted-to-cvpr-2025",
+      },{id: "news-our-paper-on-image-editing-with-diffusion-models-for-which-i-am-the-lead-author-has-been-accepted-to-cvpr-2025",
           title: 'Our paper on image editing with diffusion models, for which I am the...',
           description: "",
           section: "News",},{id: "news-our-paper-on-diffusion-bridge-models-has-been-accepted-to-kdd-2025-research-track",
           title: 'Our paper on diffusion bridge models has been accepted to KDD 2025 (Research Track)....',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
